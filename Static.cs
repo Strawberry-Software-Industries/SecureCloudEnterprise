44dc1e356987ecafc0106cad6966c75df36e8abd
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-03-25 17:30:58 +0100

    SecureCloud v2.0 Beta 3

--- Static.cs
+++ Static.cs
@@ -2,7 +2,7 @@
 {
     public class Info
     {
-        public static string Version = "2.0";
+        public static string Version = "2.0 Beta 2";
         public static string Codename = "Juicy Starfruit";
     }
 }
