88fc513c9efed1c12ec70f1ad84b67baef4b13c1
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-03-25 21:24:43 +0100

    SecureCloud Enterprise v2.0 Beta 6

--- Static.cs
+++ Static.cs
@@ -2,7 +2,7 @@
 {
     public class Info
     {
-        public static string Version = "2.0 Beta 5.1";
+        public static string Version = "2.0 Beta 6";
         public static string BuildDate = "2022-03-25";
         public static string Codename = "Juicy Starfruit";
         public static string[] Devs = { "Julian", "Matteo" };
