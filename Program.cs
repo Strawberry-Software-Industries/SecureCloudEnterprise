09576f0c06535aea6fa21a97c226669208191929
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-03-25 16:07:38 +0100

    SecureCloud Enterprise Update

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Static;
 Print("--  SecureCloud Launcher --");
 Print("SecureCloud has started!");
 Print($"Version {Info.Version} {Info.Codename}");
+Print("");
 
 
 var builder = WebApplication.CreateBuilder(args);
